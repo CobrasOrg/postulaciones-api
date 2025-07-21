7647926c6656c1efcd441321f8920c326f9de5b8
spinedace <143011658+spinedace@users.noreply.github.com>
2025-07-21 18:18:38 -0500

    fix (path): corregido error route

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -9,4 +9,4 @@ api_router = APIRouter()
 # api_router.include_router(users.router, prefix="/users", tags=["users"])
 # api_router.include_router(items.router, prefix="/items", tags=["items"])
 
-api_router.include_router(base.router, tags=["Postulaciones"])
+api_router.include_router(base.router, prefix="/base", tags=["base"]) 
